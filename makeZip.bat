7cb32e1aa341d01ba6d4f95659102c897c34e2fb
kumatan <kumatan@kumymelody.jp>
2021-09-22 07:13:50 +0900

    ビルドオプション変更

--- makeZip.bat
+++ makeZip.bat
@@ -1,9 +1,11 @@
 echo mml2vgm
 
 mkdir output
+mkdir output\NET5
 
 del /Q .\output\*.*
 xcopy .\M98\bin\Release\*.* .\output\ /E /R /Y /I /K
+xcopy .\M98DotNET_NET5\bin\Release\net5.0\*.* .\output\NET5 /E /R /Y /I /K
 del /Q .\output\*.pdb
 del /Q .\output\*.config
 copy .\CHANGE.txt .\output\
